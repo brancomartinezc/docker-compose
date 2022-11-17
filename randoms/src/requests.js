0cc68939b7eb1211878a155b86388bb360f4cafc
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-11-17 13:20:44 -0300

    docker compose + ports react

--- randoms/src/requests.js
+++ randoms/src/requests.js
@@ -1,8 +1,8 @@
 // El objetivo es reemplazar esto por microservicios que oculten la fuente de datos.
 const requests = [
-  {"url":"http://localhost:3004","color":"pink"},
-  {"url":"http://localhost:3005","color":"cyan"},
-  {"url":"http://localhost:5000","color":"yellow"},
+  {"url":`http://localhost:${process.env.EXPRESS_APP_PORT}`,"color":"pink"},
+  {"url":`http://localhost:${process.env.PHP_APP_PORT}`,"color":"cyan"},
+  {"url":`http://localhost:${process.env.PYTHON_APP_PORT}`,"color":"yellow"},
 ]
 
 export default requests;
